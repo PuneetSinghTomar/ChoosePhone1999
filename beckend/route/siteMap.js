7d33cb7072ac2ef571bc75b5ec1f6a476b150f6f
Puneet Tomar <tomarpuneet79@gmail.com>
2025-08-13 09:48:45 +0530

    changes done in index

--- beckend/route/siteMap.js
+++ beckend/route/siteMap.js
@@ -6,11 +6,9 @@ const router = express.Router();
 
 router.get("/sitemap-blog.xml", async (req, res) => {
   try {
-    res.header("Content-Type", "application/xml");
+    const blogs = await Blog.find({}, "_id updatedAt").lean();
 
-    const blogs = await Blog.find({}, "_id updatedAt"); // Only get ID and date
-
-    // Static URLs
+    // Static page URL
     const staticUrls = `
       <url>
         <loc>https://choosephone.co.in/Trends/Home</loc>
@@ -19,28 +17,41 @@ router.get("/sitemap-blog.xml", async (req, res) => {
       </url>
     `;
 
-    // Blog URLs from DB
+    // Dynamic blog URLs
     const blogUrls = blogs
-      .map(
-        (b) => `
-      <url>
-        <loc>https://choosephone.co.in/Trends/Blog/${b._id}</loc>
-        <lastmod>${new Date(b.updatedAt).toISOString()}</lastmod>
-        <priority>0.8</priority>
-      </url>
-    `
-      )
+      .map((b) => {
+        let lastmod;
+        if (b.updatedAt) {
+          try {
+            lastmod = new Date(b.updatedAt).toISOString();
+          } catch {
+            lastmod = new Date().toISOString();
+          }
+        } else {
+          lastmod = new Date().toISOString();
+        }
+
+        return `
+          <url>
+            <loc>https://choosephone.co.in/Trends/Blog/${b._id}</loc>
+            <lastmod>${lastmod}</lastmod>
+            <priority>0.8</priority>
+          </url>
+        `;
+      })
       .join("");
 
+    // Build XML
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${staticUrls}
       ${blogUrls}
     </urlset>`;
 
+    res.header("Content-Type", "application/xml");
     res.send(xml);
   } catch (error) {
-    console.error(error);
+    console.error("Error generating blog sitemap:", error);
     res.status(500).send("Error generating blog sitemap");
   }
 });
