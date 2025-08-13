95add602fee8351cc7ffe3dfc92f7053c1076e1f
Puneet Tomar <tomarpuneet79@gmail.com>
2025-08-13 09:59:57 +0530

    changes done in SiteMap

--- beckend/route/siteMap.js
+++ beckend/route/siteMap.js
@@ -9,25 +9,26 @@ router.get("/sitemap-blog.xml", async (req, res) => {
     res.header("Content-Type", "application/xml");
 
     const blogs = await Blog.find({}, "_id slug updatedAt");
-    
+
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${blogs.map(blog => `
-          <url>
-            <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
-            <lastmod>${blog.updatedAt.toISOString()}</lastmod>
-            <changefreq>weekly</changefreq>
-            <priority>0.8</priority>
-          </url>`).join("")}
-      </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${blogs
+  .map(
+    (blog) => `<url>
+  <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
+  <lastmod>${blog.updatedAt.toISOString()}</lastmod>
+  <changefreq>weekly</changefreq>
+  <priority>0.8</priority>
+</url>`
+  )
+  .join("")}
+</urlset>`;
 
     res.send(sitemap);
   } catch (err) {
-    console.error(err);
+    console.error("Sitemap generation error:", err);
     res.status(500).send("Error generating sitemap");
   }
 });
-
-
 
 export default router;
