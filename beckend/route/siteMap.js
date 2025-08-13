74a83af211d65eabde031fa0845372cfafadc071
Puneet Tomar <tomarpuneet79@gmail.com>
2025-08-13 10:07:05 +0530

    changes done in Mapping

--- beckend/route/siteMap.js
+++ beckend/route/siteMap.js
@@ -10,23 +10,33 @@ router.get("/sitemap-blog.xml", async (req, res) => {
 
     const blogs = await Blog.find({}, "_id slug updatedAt");
 
+    if (!blogs) {
+      throw new Error("No blogs found in database");
+    }
+
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${blogs
-  .map(
-    (blog) => `<url>
+  .map((blog) => {
+    if (!blog.slug || !blog.updatedAt) {
+      console.warn(
+        `Skipping blog with id ${blog._id} due to missing slug or updatedAt`
+      );
+      return "";
+    }
+    return `<url>
   <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
   <lastmod>${blog.updatedAt.toISOString()}</lastmod>
   <changefreq>weekly</changefreq>
   <priority>0.8</priority>
-</url>`
-  )
+</url>`;
+  })
   .join("")}
 </urlset>`;
 
     res.send(sitemap);
   } catch (err) {
-    console.error("Sitemap generation error:", err);
+    console.error("Error generating sitemap-blog.xml:", err);
     res.status(500).send("Error generating sitemap");
   }
 });
