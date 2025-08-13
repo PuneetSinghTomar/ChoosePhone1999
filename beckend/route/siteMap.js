18d6a3738669d58825b2514283b454b6bca4b935
Puneet Tomar <tomarpuneet79@gmail.com>
2025-08-13 09:54:27 +0530

    changes done in beckend

--- beckend/route/siteMap.js
+++ beckend/route/siteMap.js
@@ -6,54 +6,28 @@ const router = express.Router();
 
 router.get("/sitemap-blog.xml", async (req, res) => {
   try {
-    const blogs = await Blog.find({}, "_id updatedAt").lean();
-
-    // Static page URL
-    const staticUrls = `
-      <url>
-        <loc>https://choosephone.co.in/Trends/Home</loc>
-        <lastmod>${new Date().toISOString()}</lastmod>
-        <priority>1.0</priority>
-      </url>
-    `;
-
-    // Dynamic blog URLs
-    const blogUrls = blogs
-      .map((b) => {
-        let lastmod;
-        if (b.updatedAt) {
-          try {
-            lastmod = new Date(b.updatedAt).toISOString();
-          } catch {
-            lastmod = new Date().toISOString();
-          }
-        } else {
-          lastmod = new Date().toISOString();
-        }
-
-        return `
-          <url>
-            <loc>https://choosephone.co.in/Trends/Blog/${b._id}</loc>
-            <lastmod>${lastmod}</lastmod>
-            <priority>0.8</priority>
-          </url>
-        `;
-      })
-      .join("");
-
-    // Build XML
-    const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticUrls}
-      ${blogUrls}
-    </urlset>`;
-
     res.header("Content-Type", "application/xml");
-    res.send(xml);
-  } catch (error) {
-    console.error("Error generating blog sitemap:", error);
-    res.status(500).send("Error generating blog sitemap");
+
+    const blogs = await Blog.find({}, "_id slug updatedAt");
+    
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${blogs.map(blog => `
+          <url>
+            <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
+            <lastmod>${blog.updatedAt.toISOString()}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+          </url>`).join("")}
+      </urlset>`;
+
+    res.send(sitemap);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error generating sitemap");
   }
 });
+
+
 
 export default router;
