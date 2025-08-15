0b774d42987b6e7ad50b3a204f032968ff30e30a
Puneet Tomar <tomarpuneet79@gmail.com>
2025-08-15 18:06:54 +0530

    changes done in sitemap

--- beckend/route/siteMap.js
+++ beckend/route/siteMap.js
@@ -3,30 +3,25 @@ import Blog from "../model/blogModel.js";
 
 const router = express.Router();
 
+function safeDateISO(value) {
+  const d = new Date(value);
+  return isNaN(d) ? new Date().toISOString() : d.toISOString();
+}
+
 router.get("/sitemap-blog.xml", async (req, res) => {
   try {
     res.header("Content-Type", "application/xml");
 
-    const blogs = await Blog.find({}, "slug date createdAt updatedAt").lean();
+    const blogs = await Blog.find({}, "slug date createdAt updatedAt");
 
-    const blogUrls = blogs.map((blog) => {
-      let dateValue = blog.updatedAt || blog.createdAt || blog.date;
-
-      // Ensure dateValue is a Date object
-      if (!(dateValue instanceof Date)) {
-        const parsed = new Date(dateValue);
-        dateValue = isNaN(parsed) ? new Date() : parsed;
-      }
-
-      return `
-        <url>
-          <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
-          <lastmod>${dateValue.toISOString()}</lastmod>
-          <changefreq>weekly</changefreq>
-          <priority>0.8</priority>
-        </url>
-      `;
-    });
+    const blogUrls = blogs.map(blog => `
+      <url>
+        <loc>https://choosephone.co.in/Trends/Blog/${blog.slug}</loc>
+        <lastmod>${safeDateISO(blog.updatedAt || blog.createdAt || blog.date)}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+    `);
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -36,9 +31,10 @@ router.get("/sitemap-blog.xml", async (req, res) => {
 
     res.send(sitemap);
   } catch (err) {
-    console.error("Sitemap generation error:", err);
+    console.error(err);
     res.status(500).send("Error generating blog sitemap");
   }
 });
+
 
 export default router;
